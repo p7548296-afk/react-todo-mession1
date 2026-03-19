43fae94d39b6f77bff650ba048eb3930d50830e8
minseo <p7548296@gmail.com>
2026-03-19 17:05:11 +0900

    localStorage 저장, 초기 로드 추가

--- React ToDo/src/App.jsx
+++ React ToDo/src/App.jsx
@@ -1,11 +1,21 @@
+import { useEffect, useState } from 'react'
 import TodoList from './components/TodoList'
 import TodoWriteForm from './components/TodoWriteForm'
 
 function App() {
+  const [todos, setTodos] = useState(() => {
+    const savedTodos = localStorage.getItem('todos')
+    return savedTodos ? JSON.parse(savedTodos) : []
+  })
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
+
   return (
     <>
-      <TodoWriteForm />
-      <TodoList />
+      <TodoWriteForm todos={todos} setTodos={setTodos} />
+      <TodoList todos={todos} setTodos={setTodos} />
     </>
   )
 }
