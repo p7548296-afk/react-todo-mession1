3c3716d3d7844d283fb57593e529d73d08f3eefe
minseo <p7548296@gmail.com>
2026-03-19 17:29:53 +0900

    등록 후 입력창 초기화

--- React ToDo/src/components/TodoWriteForm.jsx
+++ React ToDo/src/components/TodoWriteForm.jsx
@@ -1,20 +1,27 @@
+import { useState } from 'react'
 import { useTodos } from '../context/TodoContext'
 
 function TodoWriteForm() {
   const { addTodo } = useTodos()
-  const handleOnSubmit = (e) => {
+  const [inputText, setInputText] = useState('')
+
+  const handleSubmit = (e) => {
     e.preventDefault()
-    const form = e.target
-    addTodo(form.todo.value)
+    if (inputText.trim() === '') return
+    addTodo(inputText)
+    setInputText('')
   }
 
   return (
-    <>
-      <form onSubmit={handleOnSubmit}>
-        <input type="text" name="todo" />
-        <button type="submit">등록</button>
-      </form>
-    </>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+        placeholder="할 일을 입력하세요"
+      />
+      <button type="submit">등록</button>
+    </form>
   )
 }
 
