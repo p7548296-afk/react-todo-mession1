bdf3cd9c32d12bbe7b10a97d6a56831341c69042
minseo <p7548296@gmail.com>
2026-03-19 17:24:01 +0900

    간단한 UI 수정

--- React ToDo/src/App.jsx
+++ React ToDo/src/App.jsx
@@ -1,22 +1,12 @@
-import { useEffect, useState } from 'react'
 import TodoList from './components/TodoList'
 import TodoWriteForm from './components/TodoWriteForm'
 
 function App() {
-  const [todos, setTodos] = useState(() => {
-    const savedTodos = localStorage.getItem('todos')
-    return savedTodos ? JSON.parse(savedTodos) : []
-  })
-
-  useEffect(() => {
-    localStorage.setItem('todos', JSON.stringify(todos))
-  }, [todos])
-
   return (
     <>
       <h1>! React ToDo !</h1>
-      <TodoWriteForm todos={todos} setTodos={setTodos} />
-      <TodoList todos={todos} setTodos={setTodos} />
+      <TodoWriteForm />
+      <TodoList />
     </>
   )
 }
